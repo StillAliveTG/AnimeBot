bd846fb2e63328995cc065166ca719348d79e32f
Rengamani3 <127839457+Rengamani3@users.noreply.github.com>
2023-03-16 16:48:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Animebot:<password>@anime.jimtbnm.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Animebot:Animebot@anime.jimtbnm.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Animebot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
